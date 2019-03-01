3c25ead928c928b20a48fbea570b42b486fc09d9
Ömral Cörüt <omral@ovidos.com>
2019-03-01 23:06:47 +0300

    Update Example

---
+++ app/src/main/java/com/omralcorut/linkedinsdkexample/JavaActivity.java
@@ -0,0 +1,38 @@
+package com.omralcorut.linkedinsdkexample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.omralcorut.linkedinsignin.Linkedin;
+import com.omralcorut.linkedinsignin.LinkedinLoginViewResponseListener;
+import com.omralcorut.linkedinsignin.model.LinkedinToken;
+import org.jetbrains.annotations.NotNull;
+
+public class JavaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Linkedin.Companion.initialize(getApplicationContext(),
+                "LINKEDIN_CLIENT_ID",
+                "LINKEDIN_CLIENT_SECRET",
+                "LINKEDIN_REDIRECT_URI",
+                "RANDOM_STRING");
+
+        Linkedin.Companion.login(this, new LinkedinLoginViewResponseListener() {
+            @Override
+            public void linkedinDidLoggedIn(@NotNull LinkedinToken linkedinToken) {
+                // Success
+            }
+
+            @Override
+            public void linkedinLoginDidFail(@NotNull String error) {
+                // Fail
+            }
+        });
+
+    }
+
+}
