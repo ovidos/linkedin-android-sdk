2fc75fc84686c97963250882a20fb279322ad748
Ömral Cörüt <omralcrt@gmail.com>
2019-12-01 17:48:27 +0300

    Migrate To AndroidX and Update The App Version

--- app/src/main/java/com/omralcorut/linkedinsdkexample/JavaActivity.java
+++ app/src/main/java/com/omralcorut/linkedinsdkexample/JavaActivity.java
@@ -1,8 +1,8 @@
 package com.omralcorut.linkedinsdkexample;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import com.omralcorut.linkedinsignin.Linkedin;
 import com.omralcorut.linkedinsignin.LinkedinLoginViewResponseListener;
 import com.omralcorut.linkedinsignin.model.LinkedinToken;
--- app/src/main/java/com/omralcorut/linkedinsdkexample/MainActivity.kt
+++ app/src/main/java/com/omralcorut/linkedinsdkexample/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.omralcorut.linkedinsdkexample
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.omralcorut.linkedinsignin.Linkedin
 import com.omralcorut.linkedinsignin.LinkedinLoginViewResponseListener
